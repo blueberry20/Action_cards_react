bd67d3c679afd76b86b83df3eef978254dd0ae6a
blueberry20 <olgasales@gmail.com>
2018-08-11 15:04:48 -0400

    fixed syntax error

--- src/components/memory_game.js
+++ src/components/memory_game.js
@@ -157,7 +157,7 @@ function shuffle(array) {
         randomIndex;
 
     // While there remain elements to shuffle...
-    while (0 !=== currentIndex) {
+    while (0 !== currentIndex) {
         // Pick a remaining element...
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
